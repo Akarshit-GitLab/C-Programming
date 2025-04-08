738c179ae13a8755359ac8a3529df9158d0216d1
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-04-08 09:57:21 +0530

    Source code

---
+++ Function & Pointer/permutation_combination.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int factorial(int n){
+    int fact =1;
+    for(int i =2;i<=n;i++){
+        fact = fact*i;
+    }
+    return fact;
+}
+int main(){
+    int n,r;
+    printf("Enter the n = ");
+    scanf("%d",&n);
+    printf("enter the r = ");
+    scanf("%d",&r);
+    int ncr =1;
+    printf("NCR = %d ",factorial(n)/(factorial(r)*factorial(n-r)));
+
+}
